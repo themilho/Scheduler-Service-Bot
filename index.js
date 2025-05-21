3bdb3ee569c5badae333d697f18fe9d834cbfa56
themilho <the.milho@outlook.com>
2025-05-20 21:04:56 -0300

    Configurando variáveis de ambiente

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require ("dotenv").config();
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const axios = require("axios");
+const {Telegraf} = require("telegraf");
+const {message} = require("telegraf/filters");
 
 const app = express();
-app.use(bodyParser.json())
+app.use(express.json())
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Bem-vindo!'))
+bot.launch()
+
 
 app.listen(process.env.PORT || 3000, async () => {
     console.log("app running on port", process.env.PORT || 3000);    
