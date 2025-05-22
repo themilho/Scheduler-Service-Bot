74530b10cb92074d87b5e10d0fcdc94bcfa67fd4
themilho <the.milho@outlook.com>
2025-05-22 19:20:52 -0300

    Setando webhook com a API do Telegram

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-require ("dotenv").config();
+require("dotenv").config();
 
 const express = require("express");
 const axios = require("axios");
-const {Telegraf, Markup} = require("telegraf");
-const {message} = require("telegraf/filters");
-const {urlNgrok, botToken} = process.env
+const { Telegraf, Markup } = require("telegraf");
+const { message } = require("telegraf/filters");
+const { URL_NGROK, BOT_TOKEN } = process.env
 
-const bot = new Telegraf(botToken)
+const bot = new Telegraf(BOT_TOKEN, { polling: false })
 const app = express();
 app.use(express.json())
-const telegramApi = `https://api.telegram.org/bot${botToken}`
 
-// const webHookUrl = urlNgrok + botToken
+const telegramApi = `https://api.telegram.org/bot${BOT_TOKEN}/setWebhook`
+const webhookUrl = URL_NGROK + '/webhook'
 
+async function setWebhook() {
+    try {
+        const response = await axios.post(telegramApi, {url: webhookUrl});
+        if (response.data.ok){
+            console.log("Webhook setado com sucesso");
+        }else{
+            console.error("Erro ao setar webhook", response.data)
+        }
+    } catch (error) {
+        console.error('Erro ao fazer a requisição: ', error)
+    }
+}
 
+app.post('/webhook', (req, res) => {
+    bot.handleUpdate(req.body);
+    res.sendStatus(200);
+})
+
+bot.start((ctx) => ctx.reply('welcome'))
 
 app.listen(process.env.PORT || 3000, async () => {
-    console.log("app running on port", process.env.PORT || 3000);    
+    console.log("app running on port", process.env.PORT || 3000);
+    setWebhook();
 })
 
